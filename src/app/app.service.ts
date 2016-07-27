3cb4cd8e574a87d21286df1859c7560a2f30e378
Jenny Fung <jfung@azavea.com>
2016-07-27 10:56:02 -0400

    Revert "Scaffold project in Angular"
    This reverts commit b849623be8e2a042c5451121bd66d08251685b22.

--- src/app/app.service.ts
+++
@@ -1,6 +0,0 @@
-import {HmrState} from 'angular2-hmr';
-
-export class AppState {
-  // @HmrState() is used by HMR to track the state of any object during a hot module replacement
-  @HmrState() _state = { };
-}
