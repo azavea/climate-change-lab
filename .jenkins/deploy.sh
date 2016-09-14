a1b9f9c9be8bc28c1aa9167cd6c9fea1610c0fb4
CloudNiner <andrew@cloudniner.co>
2016-09-14 14:54:12 -0400

    Add jenkins deploy script
    This rebuilds the prod version of the project and pushes
    it via s3_website

---
+++ .jenkins/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# The constants.ts file is templated in the jenkins build configuration
+# The ENV for the s3_website deploy is set in the jenkins build configuration
+
+set -e
+set -x
+
+vagrant up && vagrant ssh -c "
+  cd /opt/climate-change-lab
+  npm install
+  npm rebuild node-sass
+  npm run build:prod
+  s3_website push
+"
