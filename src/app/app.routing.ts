24f4b00528d717d3e353660d405cb723d8c05df5
Jenny Fung <jfung@azavea.com>
2016-09-23 16:36:45 -0400

    Reroute dashboard links to new project form

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LabComponent } from './lab/lab.component';
 import { LoginComponent } from './login/login.component';
+import { AddEditProjectComponent } from './project/add-edit-project.component';
 import { PageNotFoundComponent } from './http-status/page-not-found/page-not-found.component';
 
 import { AuthGuard } from './auth/auth.guard';
@@ -11,6 +12,7 @@ import { AuthGuard } from './auth/auth.guard';
 const appRoutes: Routes = [
   { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'lab', component: LabComponent, canActivate: [AuthGuard] },
+  { path: 'new', component: AddEditProjectComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
