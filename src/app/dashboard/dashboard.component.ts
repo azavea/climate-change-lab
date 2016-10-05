20e4a071107bbcfd83a766c9bb26798f1ccaba1b
Jenny Fung <jfung@azavea.com>
2016-10-05 12:49:25 -0400

    Paginate projects, add bootstrap

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,11 +10,16 @@ import { ProjectService } from '../services/project.service';
 export class DashboardComponent implements OnInit {
 
     private projects: Project[];
+    private pageOfProjects: Project[];
+    public totalItems: number;
+    public currentPage: number = 1;
 
     constructor(private projectService: ProjectService) {}
 
     ngOnInit() {
         this.getProjects();
+        this.totalItems = this.projects.length;
+        this.setPage();
     }
 
     public getProjects() {
@@ -24,5 +29,17 @@ export class DashboardComponent implements OnInit {
     public deleteProject(project) {
         this.projectService.remove(project);
         this.getProjects();
+        this.setPage();
     }
+
+    public setPage() {
+        this.pageOfProjects = this.projects.slice(0, 5);
+    }
+
+    public pageChanged(event:any):void {
+        let index = event.itemsPerPage * (event.page - 1);
+        this.pageOfProjects = this.projects.slice(index, index + event.itemsPerPage);
+        console.log('Page changed to: ' + event.page);
+        console.log('Number items per page: ' + event.itemsPerPage);
+    };
 }
