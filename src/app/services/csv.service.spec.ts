0fb35a425e53c7c571067dab856ab0e2458a4e10
Kathryn Killebrew <kkillebrew@azavea.com>
2017-01-19 11:44:28 -0500

    Add test for CSV data conversion
    Test CSV service conversion from JSON to CSV with header row for download.

---
+++ src/app/services/csv.service.spec.ts
@@ -0,0 +1,31 @@
+import { CSVService }    from './csv.service';
+
+
+describe('CSVService', () => {
+
+    let service: CSVService;
+
+    let testData = [{data: [
+         {date: 'Sun Jan 01 2006 00:00:00 GMT-0500 (EST)', values: {avg: 1, max: 2, min: 0}},
+         {date: 'Mon Jan 01 2007 00:00:00 GMT-0500 (EST)', values: {avg: 42.0, max: 111.1, min: -2.2}}],
+        indicator: {name: 'extreme_cold_events'}, time_aggregation: 'yearly', time_format: '%Y'}];
+
+    let testResult = [
+        'date,extreme_cold_events-undefined-avg,extreme_cold_events-undefined-max,extreme_cold_events-undefined-min',
+        'Sun Jan 01 2006 00:00:00 GMT-0500 (EST),1,2,0',
+        'Mon Jan 01 2007 00:00:00 GMT-0500 (EST),42,111.1,-2.2'
+    ].join('\r\n');
+
+    beforeEach(() => {
+        service = new CSVService();
+    });
+
+    it('should have a downloadAsCSV method', () => {
+        expect(service.downloadAsCSV).toBeDefined();
+    });
+
+    it('should convert data to tabular format', () => {
+        let tabularData = service.dataToCSV(testData);
+        expect(tabularData).toBe(testResult);
+    });
+});
