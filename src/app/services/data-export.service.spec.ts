86c8642a9a2c47ce827785f01868bc4fdbb1e4d8
Kathryn Killebrew <kkillebrew@azavea.com>
2017-08-01 12:30:12 -0400

    Update data export tests

--- src/app/services/data-export.service.spec.ts
+++ src/app/services/data-export.service.spec.ts
@@ -1,33 +1,15 @@
-import { CSVService } from './csv.service';
+import { DataExportService } from './data-export.service';
 
 
 describe('CSVService', () => {
 
-    let service: CSVService;
-
-    /* tslint:disable:max-line-length */
-    const testData = [{data: [
-         {date: 'Sun Jan 01 2006 00:00:00 GMT-0500 (EST)', values: {avg: 1, max: 2, min: 0}},
-         {date: 'Mon Jan 01 2007 00:00:00 GMT-0500 (EST)', values: {avg: 42.0, max: 111.1, min: -2.2}}],
-        indicator: {name: 'extreme_cold_events'}, time_aggregation: 'yearly', time_format: '%Y'}];
-
-    const testResult = [
-        'date,extreme_cold_events-undefined-avg,extreme_cold_events-undefined-max,extreme_cold_events-undefined-min',
-        'Sun Jan 01 2006 00:00:00 GMT-0500 (EST),1,2,0',
-        'Mon Jan 01 2007 00:00:00 GMT-0500 (EST),42,111.1,-2.2'
-    ].join('\r\n');
-    /* tslint:enable:max-line-length */
+    let service: DataExportService;
 
     beforeEach(() => {
-        service = new CSVService();
+        service = new DataExportService();
     });
 
-    it('should have a downloadAsCSV method', () => {
-        expect(service.downloadAsCSV).toBeDefined();
-    });
-
-    it('should convert data to tabular format', () => {
-        const tabularData = service.dataToCSV(testData);
-        expect(tabularData).toBe(testResult);
+    it('should have a downloadAsJSON method', () => {
+        expect(service.downloadAsJSON).toBeDefined();
     });
 });
