f616b486c0e48bf2c705f69ca8be837aae63e3f2
Kathryn Killebrew <kkillebrew@azavea.com>
2017-08-01 12:19:10 -0400

    Remove CSV download option
    Use JSON download option now instead. Also remove now-unused papaparse library.

--- src/app/services/data-export.service.ts
+++ src/app/services/data-export.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import 'rxjs/Rx';
-import * as Papa from 'papaparse';
 import * as _ from 'lodash';
 import * as FileSaver from 'file-saver';
 
@@ -9,21 +8,6 @@ import * as FileSaver from 'file-saver';
  */
 @Injectable()
 export class DataExportService {
-
-    public dataToCSV(data): string {
-        const label = data[0].indicator.name + '-' + data[0].indicator.default_units;
-        const body = _(data[0].data)
-                       .map((row) => _.extend({ date: row.date }, row.values))
-                       .map((row) => _.mapKeys(row, (value, key) =>
-                                                    key === 'date' ? 'date' : label + '-' + key
-                                              )).value();
-        return Papa.unparse(body);
-    }
-
-    public downloadAsCSV(data): void {
-        const filename = data[0].indicator.name + '.csv';
-        this.downloadFile(this.dataToCSV(data), filename, 'text/csv');
-    }
 
     public downloadAsJSON(data): void {
         const filename = data[0].indicator.name + '.json';
