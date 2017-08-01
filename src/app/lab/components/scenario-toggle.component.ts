7180b1edf658608f3f65cfa37ed855dfeff5e9c2
CloudNiner <andrew@cloudniner.co>
2017-08-01 15:32:17 -0400

    Fixup button radius in scenario toggle button group

--- src/app/lab/components/scenario-toggle.component.ts
+++ src/app/lab/components/scenario-toggle.component.ts
@@ -15,7 +15,7 @@ import { ScenarioService } from '../../services/scenario.service';
 @Component({
   selector: 'ccl-scenario-toggle',
   template: `
-    <div class="dropdown">
+    <div class="dropdown button-group">
         <button class="button"
                *ngFor="let s of scenarios"
                [ngClass]="{'active': s.name === projectData.scenario.name}"
@@ -31,6 +31,7 @@ export class ScenarioToggleComponent implements OnInit {
     @Input() projectData: ProjectData;
     public scenarios: Scenario[] = [];
     private DEFAULT_SCENARIO_NAME = 'RCP85';
+    private VALID_SCENARIOS = ['RCP85', 'RCP45'];
 
     constructor(private scenarioService: ScenarioService) {}
 
@@ -44,7 +45,7 @@ export class ScenarioToggleComponent implements OnInit {
 
     private getScenarios() {
         this.scenarioService.list().subscribe(data => {
-            this.scenarios = data.filter(s => s.name === 'RCP85' || s.name === 'RCP45')
+            this.scenarios = data.filter(s => this.VALID_SCENARIOS.includes(s.name));
 
             // Set a default for the project if none is set
             if (!this.projectData.scenario.label) {
