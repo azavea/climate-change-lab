9eed5e91afce96f085581b548d92d3fc1ae6cd79
Kathryn Killebrew <kkillebrew@azavea.com>
2017-09-19 17:19:48 -0400

    Add toggle for dataset
    Adds toggle control to allow for selecting which dataset to use.
    Currently toggling it does nothing.
    
    Closes #258.

---
+++ src/app/lab/components/dataset-toggle.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Dataset } from '../../models/dataset.model';
+import { ProjectData } from '../../models/project-data.model';
+import { DatasetService } from '../../services/dataset.service';
+
+/*  Dataset Toggle Component
+
+    -- Requires project input
+    Expected use:
+        <ccl-dataset-toggle
+            [projectData]="your_project.project_data">
+*/
+
+@Component({
+  selector: 'ccl-dataset-toggle',
+  template: `
+    <div class="dropdown button-group">
+        <button class="button"
+               *ngFor="let s of datasets"
+               [ngClass]="{'active': s.name === projectData.dataset.name}"
+               (click)="onDatasetClicked(s, $event)">
+            {{ s.name }}
+        </button>
+    </div>
+  `
+
+})
+export class DatasetToggleComponent implements OnInit {
+
+    @Input() projectData: ProjectData;
+    public datasets: Dataset[] = [];
+    private DEFAULT_DATASET_NAME = 'NEX-GDDP';
+
+    constructor(private datasetService: DatasetService) {}
+
+    ngOnInit() {
+        this.getDatasets();
+    }
+
+    public onDatasetClicked(dataset: Dataset, event?: Event) {
+        this.projectData.dataset = dataset;
+        if (event) {
+            event.preventDefault();
+        }
+    }
+
+    private getDatasets() {
+        this.datasetService.list().subscribe(data => {
+            this.datasets = data;
+
+            // Set a default for the project if none is set
+            if (!this.projectData.dataset) {
+                this.onDatasetClicked(this.datasets.find((s) => {
+                    return s.name === this.DEFAULT_DATASET_NAME;
+                }));
+            }
+        });
+    }
+}
