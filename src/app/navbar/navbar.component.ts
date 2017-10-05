f5a6bffbecff9a63b867e680dfdae6c13d117cc3
CloudNiner <andrew@cloudniner.co>
2017-10-05 11:32:27 -0400

    Update lab to use universal header
    As screnshotted here:
    https://github.com/azavea/climate-change-lab/issues/271#issue-262795758
    
    Did not switch to "First Last" in dropdown button because
    those fields can be blank and we're not currently pulling
    the values down. Email address seems like a more compelling
    value to show.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { AuthService } from '../auth/auth.service';
+
+import { apiHost } from '../constants';
 
 
 @Component({
   selector: 'ccl-navbar',
   templateUrl: './navbar.component.html'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
     @Input() projectID: string;
 
     public isSettingsDropdownOpen = false;
+    public apiHost = '';
 
     constructor(public authService: AuthService) {}
+
+    ngOnInit() {
+      const lowercaseApiHost = apiHost.toLowerCase();
+      if (lowercaseApiHost.indexOf('localhost') !== -1) {
+        this.apiHost = 'localhost:8080';
+      } else if (lowercaseApiHost.indexOf('staging') !== -1) {
+        this.apiHost = 'app.staging.climate.azavea.com';
+      } else {
+        this.apiHost = 'app.climate.azavea.com';
+      }
+    }
 }
